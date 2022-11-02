940319174fdad0ad1873d688fe69a9c2abb52988
rajesh <rajesh@hcl.com>
2022-11-02 06:38:14 +0000

    Updated partner search and master containner

--- force-app/main/default/lwc/partnerSearch/partnerSearch.js
+++ force-app/main/default/lwc/partnerSearch/partnerSearch.js
@@ -1,8 +1,14 @@
-
+import { LightningElement } from "lwc";
 
 export default class PartnerSearch extends LightningElement {
+    value = 'inProgress';
 
-
-   
+    get options() {
+        return [
+            { label: 'New', value: 'new' },
+            { label: 'In Progress', value: 'inProgress' },
+            { label: 'Finished', value: 'finished' },
+        ];
+    }
     
 }
