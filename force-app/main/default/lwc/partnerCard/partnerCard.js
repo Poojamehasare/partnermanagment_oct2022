20dda5306b4769012e64450257898561147a90c0
readervm-Rajesh <reader.vm@gmail.com>
2022-11-04 11:18:24 +0530

    Updated Partner Card

--- force-app/main/default/lwc/partnerCard/partnerCard.js
+++ force-app/main/default/lwc/partnerCard/partnerCard.js
@@ -4,8 +4,14 @@ import { LightningElement, api, wire } from 'lwc';
 
 export default class PartnerCard extends LightningElement {
 
+    // JS Object
+    partnerAccount = {
 
-
+        userImg : 'https://randomuser.me/api/portraits/med/men/75.jpg',
+        Name: 'Salesforce Inc',
+        Partner_Primary_POC: 'Kate Willamson',
+        Partner_Type: 'Technology Partner',
+    };
 
 
 }
