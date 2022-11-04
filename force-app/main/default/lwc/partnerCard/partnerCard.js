f63fe18eb594fb0cbb11de53fac69a3dd5f4d493
rajesh <rajesh@hcl.com>
2022-11-04 06:11:03 +0000

    Updated PArtner Card

--- force-app/main/default/lwc/partnerCard/partnerCard.js
+++ force-app/main/default/lwc/partnerCard/partnerCard.js
@@ -7,11 +7,38 @@ export default class PartnerCard extends LightningElement {
     // JS Object
     partnerAccount = {
 
-        userImg : 'https://randomuser.me/api/portraits/med/men/75.jpg',
+        userImg: 'https://randomuser.me/api/portraits/med/men/75.jpg',
         Name: 'Salesforce Inc',
         Partner_Primary_POC: 'Kate Willamson',
-        Partner_Type: 'Technology Partner',
+        Partner_Type: 'Social Media Channel Partner',
     };
+
+
+    partnerBadgeTheme = 'slds-theme_offline';
+
+    connectedCallback() {
+
+        // get the theme dynamically
+        this.getPartnerTheme(this.partnerAccount.Partner_Type);
+    }
+
+    getPartnerTheme(partnerType) {
+        switch (partnerType) {
+            case "Technology Partner":
+                this.partnerBadgeTheme = 'slds-theme_success';
+                break;
+            case "Marketing Partner":
+                this.partnerBadgeTheme = 'slds-theme_info';
+                break;
+            case "Social Media Channel Partner":
+                this.partnerBadgeTheme = 'slds-theme_warning';
+                break;
+
+            default:
+                this.partnerBadgeTheme = 'slds-theme_offline';
+                break;
+        }
+    }
 
 
 }
